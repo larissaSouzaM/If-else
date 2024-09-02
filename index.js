4896db8d89fa85dd584c5c263984b6471a312934
Larissa de Souza Mendes <161725440+larissaSouzaM@users.noreply.github.com>
2024-09-02 13:12:01 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+console.log`tranalhando com condicionais`);
+console.log`destinos oferecidos:`);
+const ListaDestinos = new Array
+`Viatna`,
+`Moscou`,
+`toquio`,
+`canada`
+);
+const idadeComprador = 16;
+  if(idadeComprador < 18){
+console.log("Comprador menor de idade nao podemos vender as passagens.");
+    }else {
+    console.log(`Qusl a forma de pagamento?`);
+    console.log(`boa viagem`);
+    }
